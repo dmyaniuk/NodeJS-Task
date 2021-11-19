e6bd8a02d931b81b6c87c822f1bfe58790c64a9e
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-11-19 15:14:46 +0300

    Add helmet to make sure app doesn't send any description about server

--- src/middleware/http-pipeline.middleware.ts
+++ src/middleware/http-pipeline.middleware.ts
@@ -1,7 +1,9 @@
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
 import { Express } from 'express';
 
 const registerHttpPipeline = (app: Express): void => {
+    app.use(helmet());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 };
